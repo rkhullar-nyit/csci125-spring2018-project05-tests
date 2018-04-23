104dc29717f3021d7299da3a9ee633cdf396c704
Rajan Khullar <rkhullar03@gmail.com>
2018-04-22 23:47:03 -0400

    added member tests for rectangle sorter; added swap test

--- RectangleSorterTest.java
+++ RectangleSorterTest.java
@@ -1,4 +1,116 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
-public class RectangleSortertest {
+import java.lang.reflect.Array;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class RectangleSorterTest extends RectangleSorterTestBase
+{
+
+    @Test
+    public void test_canary()
+    {
+        assertThat(1, is(equalTo(1)));
+    }
+
+    @Test
+    public void test_rectangle_exists()
+    {
+        assertThat(RectangleClass, is(notNullValue()));
+    }
+
+    @Test
+    public void test_rectangle_sorter_exists()
+    {
+        assertThat(RectangleSorterClass, is(notNullValue()));
+    }
+
+    @Test
+    public void test_rectangle_sorter_constructor() throws Exception
+    {
+        assertThat(RectangleSorterFull, is(notNullValue()));
+        assertThat(RectangleSorterFull, hasModifiers(PUBLIC));
+
+        Object array = new_rectangle_array(0);
+        Object dut = RectangleSorterFull.newInstance(array);
+        assertThat(dut, is(notNullValue()));
+        assertThat(RectangleSorterArray.get(dut), is(notNullValue()));
+    }
+
+    @Test
+    public void test_rectangle_sorter_members()
+    {
+        assertThat(RectangleSorterArray, is(notNullValue()));
+        assertThat(RectangleSorterArray.getType(), is(equalTo(RectangleArrayClass)));
+        assertThat(RectangleSorterArray, hasModifiers(PRIVATE|PROTECTED));
+    }
+
+    @Test
+    public void test_rectangle_sorter_method_signatures()
+    {
+        assertThat(RectangleSorterSort, is(notNullValue()));
+        assertThat(RectangleSorterSort.getReturnType(), is(equalTo(void.class)));
+        assertThat(RectangleSorterSort, hasModifiers(PUBLIC));
+
+        assertThat(RectangleSorterMin, is(notNullValue()));
+        assertThat(RectangleSorterMin.getReturnType(), is(equalTo(int.class)));
+        assertThat(RectangleSorterMin, hasModifiers(PRIVATE|PROTECTED));
+
+        assertThat(RectangleSorterSwap, is(notNullValue()));
+        assertThat(RectangleSorterSwap.getReturnType(), is(equalTo(void.class)));
+        assertThat(RectangleSorterSwap, hasModifiers(PRIVATE|PROTECTED));
+    }
+
+    private Object new_rectangle(int width, int height) throws Exception
+    {
+        Object rectangle = RectangleClass.newInstance();
+        RectangleWidth.set(rectangle, width);
+        RectangleHeight.set(rectangle, height);
+        return rectangle;
+    }
+
+    private Object new_rectangle_array(int size)
+    {
+        return Array.newInstance(RectangleClass, size);
+    }
+
+    private Object new_rectangle_array(int[][] dimension_array) throws Exception
+    {
+        Object array = new_rectangle_array(dimension_array.length);
+        for(int i=0; i<dimension_array.length; i++)
+        {
+            int[] dimensions = dimension_array[i];
+            Object rectangle = new_rectangle(dimensions[0], dimensions[1]);
+            Array.set(array, i, rectangle);
+        }
+        return array;
+    }
+
+    private Object new_rectangle_sorter(Object array) throws Exception
+    {
+        Object sorter = RectangleSorterFull.newInstance(array);
+        RectangleSorterArray.set(sorter, array);
+        return sorter;
+    }
+
+    @Test
+    public void test_swap() throws Exception
+    {
+        int[][] dimension_array = {{0,0}, {1,1}, {2,2}};
+        Object array = new_rectangle_array(dimension_array);
+
+        int i=0, j=2;
+        Object A = Array.get(array, i);
+        Object B = Array.get(array, j);
+
+        Object dut = new_rectangle_sorter(array);
+        RectangleSorterSwap.invoke(dut, i, j);
+
+        assertThat(Array.get(array, i), is(equalTo(B)));
+        assertThat(Array.get(array, j), is(equalTo(A)));
+    }
+
+
+
 }
--- RectangleSorterTestBase.java
+++ RectangleSorterTestBase.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import tools.reflection.*;
 
-public class RectangleSorterTestBase {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class RectangleSorterTestBase extends ReflectTest
+{
+
+    protected Class RectangleClass, RectangleSorterClass, RectangleArrayClass;
+    protected Field RectangleWidth, RectangleHeight, RectangleSorterArray;
+    protected Method RectangleSorterMin, RectangleSorterSwap, RectangleSorterSort;
+    protected Constructor RectangleSorterFull;
+
+    protected ReflectTestAdapter buildReflectAdapter()
+    {
+        return new ReflectTestAdapter()
+        {
+            public void init()
+            {
+                reflector = new MultiReflector();
+            }
+
+            public void load()
+            {
+                MultiReflector multi = (MultiReflector) reflector;
+                multi.add("Rectangle");
+                multi.add("RectangleSorter");
+
+                SingleReflector sorter = multi.fetch("RectangleSorter");
+                sorter.init_fields("array");
+                sorter.init_method("min", int.class);
+                sorter.init_method("swap", int.class, int.class);
+                sorter.init_method("sort");
+
+                RectangleClass = multi.fetch("Rectangle").fetch_class();
+                RectangleArrayClass = Reflector.as_array(RectangleClass);
+                sorter.init_constructor("full", RectangleArrayClass);
+
+                SingleReflector rectangle = multi.fetch("Rectangle");
+                rectangle.init_fields("width", "height");
+            }
+
+            public void link()
+            {
+                MultiReflector multi = (MultiReflector) reflector;
+                SingleReflector primary = multi.fetch("RectangleSorter");
+                SingleReflector rectangle = multi.fetch("Rectangle");
+
+                RectangleClass = rectangle.fetch_class();
+                RectangleSorterClass = primary.fetch_class();
+
+                RectangleWidth = rectangle.fetch_field("width");
+                RectangleHeight = rectangle.fetch_field("height");
+
+                RectangleSorterArray = primary.fetch_field("array");
+                RectangleSorterMin = primary.fetch_method(Alias.build("min"));
+                RectangleSorterSwap = primary.fetch_method(Alias.build("swap"));
+                RectangleSorterSort = primary.fetch_method(Alias.build("sort"));
+                RectangleSorterFull = primary.fetch_constructor(Alias.from_nick("full"));
+            }
+        };
+    }
 }
